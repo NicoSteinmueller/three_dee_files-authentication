03b072560d0abf82c6fa2141879635f58df2bc83
Nico Steinmüller <nico.steinmueller@gmx.net>
2023-03-09 08:52:33 +0100

    init

---
+++ src/main/java/com/three_dee_files/authentification/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.three_dee_files.authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
+    }
+
+}
